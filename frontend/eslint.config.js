85aff4a21e41744468aeb989f0651d037da6adb9
Emeka Machie <emmyjones4u@gmail.com>
2026-04-23 01:10:32 +0000

    Fix ESLint v9 config format

---
+++ frontend/eslint.config.js
@@ -0,0 +1,16 @@
+import js from "@eslint/js";
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: {
+        require: "readonly",
+        process: "readonly",
+        console: "readonly",
+        __dirname: "readonly",
+      }
+    }
+  }
+];
